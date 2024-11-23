79167f76d50267272361a33ce8ca80007164c3a5
Shubham Mishra <shubhammishra10101998@gmail.com>
2024-11-23 18:58:54 +0530

    Creates navbar and configured ImageKit

---
+++ client/src/components/Image.jsx
@@ -0,0 +1,16 @@
+import { IKImage } from 'imagekitio-react';
+
+export default function Image({ src, className, w, h, alt }) {
+  return (
+    <IKImage
+      urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
+      path={src}
+      className={className}
+      loading="lazy"
+      lqip={{ active: true, quality: 20 }}
+      alt={alt}
+      width={w}
+      height={h}
+    />
+  );
+}
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+// IMAGE KIT
+import Image from './Image';
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* ------ LOGO ------ */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="logo.png" alt='Shubh Logo' w={32} h={32} />
+        <span>Shubhlog</span>
+      </div>
+      {/* ------ MOBILE MENU ------ */}
+      <div className="md:hidden">
+        {/* ------ MOBILE LINK LIST ------ */}
+        <div className="cursor-pointer text-2xl" onClick={() => setOpen(!open)}>
+          {open ? 'X' : '☰'}
+        </div>
+        {/* ------ MOBILE LINK LIST ------ */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 ${
+            open ? '-right-0' : '-right-[100%]'
+          } transition-all ease-in-out gap-8 font-medium text-lg`}
+        >
+          {/* LINKS */}
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* ------ DESKTOP MENU ------ */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        {/* LINKS */}
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
