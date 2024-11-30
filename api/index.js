04b68fbe82a7628a848cc3589a3d2e3135ef175a
Shubham Mishra <shubhammishra10101998@gmail.com>
2024-11-30 13:39:05 +0530

    Add backend to the codebase

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
+import commentRouter from './routes/comment.route.js';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// ROUTES
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
+app.use('/comments', commentRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
