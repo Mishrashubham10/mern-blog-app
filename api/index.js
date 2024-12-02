cadf6d51918f1ab977d1a5bae72bac0cb5ca3eb8
Shubham Mishra <shubhammishra10101998@gmail.com>
2024-12-02 12:07:26 +0530

    creates PostController

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import userRouter from './routes/user.route.js';
 import postRouter from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
@@ -9,7 +11,17 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // MIDDLEWARES
-app.use(express.json())
+app.use(express.json());
+
+// ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message || 'Something went wrong',
+    status: err.status,
+    stack: err.stack,
+  });
+});
 
 // ROUTES
 app.use('/users', userRouter);
--- api/lib/connectDB.js
+++ api/lib/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log(`Connected to MongoDB !! HOST !!: ${conn.connection.host}`);
   } catch (err) {
     console.log('Error while connecting to Database', err);
